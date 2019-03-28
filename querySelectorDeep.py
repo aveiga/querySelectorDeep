c07c94801518ed9757308473900091e798b32c37
André Veiga <aveiga@Andres-MBP.lan>
2019-03-28 00:07:06 +0000

    adding querySelectorAllDeep; search now supports nested paths, eg: search by 'demo-view div a'

--- querySelectorDeep.py
+++ querySelectorDeep.py
@@ -15,6 +15,11 @@ def printNodeNames(nodesList):
         print("Got an error while printing the node tag name")
 
 
+def conditionalPrintNodeNames(condition, nodesList):
+    if condition:
+        printNodeNames(nodesList)
+
+
 def isBlackListed(node):
     try:
         if node.tag_name == "script":
@@ -27,42 +32,84 @@ def isBlackListed(node):
     return False
 
 
-def querySelectorDeep(initialNode, match):
+def conditionalPrint(condition, value):
+    if condition:
+        print(value)
+
+
+def evaluateMatch(n, m):
+    "This is the function that should be extended to have other kinds of matches (By class? By id? By attribute?)"
+    return n.tag_name == m
+
+
+def querySelectorDeep(initialNode, match, all=False, debug=False):
     "This traverses the DOM Tree, including shadowRoot, searching for a match"
     toCheck = [initialNode]
-    i = 0
-    while len(toCheck) > 0:
-        node = toCheck.pop(0)
+    partialFindings = [initialNode]
+    matches = match.split(" ")
 
-        if isBlackListed(node):
-            continue
+    for m in matches:
+        conditionalPrint(debug, "Looking at match: " + m)
+        i = 0
+        toCheck = partialFindings
+        partialFindings = []
+        conditionalPrintNodeNames(debug, toCheck)
+        conditionalPrintNodeNames(debug, partialFindings)
+        toCheckLength = len(toCheck)
 
-        try:
-            print("Looking at: " + node.tag_name)
+        while len(toCheck) > 0:
+            node = toCheck.pop(0)
 
-            if node.tag_name == match:
-                print("FOUND MATCH: " + node.tag_name)
-                return node
+            if isBlackListed(node):
+                continue
 
-            lightChildren = node.find_elements_by_xpath('./*')
-            shadowRoot = chrome.execute_script(
-                'return arguments[0].shadowRoot', node)
+            try:
+                conditionalPrint(debug, "Looking at: " + node.tag_name)
 
-            if len(lightChildren) > 0:
-                print(len(lightChildren))
-                toCheck.extend(lightChildren)
+                if evaluateMatch(node, m):
+                    conditionalPrint(debug, "FOUND MATCH: " + node.tag_name)
+                    if not all:
+                        return node
+                    partialFindings.extend([node])
 
-            if shadowRoot != None:
-                shadowChildren = chrome.execute_script(
-                    'return arguments[0].children', shadowRoot)
-                print(len(shadowChildren))
-                toCheck.extend(shadowChildren)
-        except:
-            print("Got an error while traversing the DOM Tree for node: " + node.tag_name)
+                lightChildren = node.find_elements_by_xpath('./*')
+                shadowRoot = chrome.execute_script(
+                    'return arguments[0].shadowRoot', node)
 
-        printNodeNames(toCheck)
-    print("not found...")
-    return 0
+                if len(lightChildren) > 0:
+                    conditionalPrint(debug, len(lightChildren))
+                    toCheck.extend(lightChildren)
+
+                if shadowRoot != None:
+                    shadowChildren = chrome.execute_script(
+                        'return arguments[0].children', shadowRoot)
+                    conditionalPrint(debug, len(shadowChildren))
+                    toCheck.extend(shadowChildren)
+            except:
+                print(
+                    "Got an error while traversing the DOM Tree for node: " + node.tag_name)
+
+            conditionalPrintNodeNames(debug, toCheck)
+    # print("not found...")
+    return partialFindings
 
 
-querySelectorDeep(chrome.find_element(By.XPATH, '//body'), "a")
+def querySelectorAllDeep(initialNode, match, debug=False):
+    return querySelectorDeep(initialNode, match, True, debug)
+
+
+print("Using querySelectorDeep----------")
+result = querySelectorDeep(chrome.find_element(
+    By.XPATH, '//body'), "demo-view")
+printNodeNames([result])
+
+
+print("Using querySelectorAllDeep----------")
+result = querySelectorAllDeep(chrome.find_element(
+    By.XPATH, '//body'), "demo-view a")
+printNodeNames(result)
+
+print("Using querySelectorDeep in debug mode----------")
+result = querySelectorDeep(chrome.find_element(
+    By.XPATH, '//body'), "demo-view", False, True)
+printNodeNames([result])
